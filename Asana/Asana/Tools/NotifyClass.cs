e1ac23ce077a0b8b58825b955f59193b970bf078
Nubar Khalidova <nkh.nubarkhalid@gmail.com>
2018-08-05 17:33:14 +0300

    Initial Commit

---
+++ Asana/Asana/Tools/NotifyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Tools
+{
+    
+        [Serializable]
+        public class NotifyClass : INotifyPropertyChanged
+        {
+            [NonSerialized]
+            private PropertyChangedEventHandler _PropertyChanged;
+
+            public virtual event PropertyChangedEventHandler PropertyChanged
+            {
+                add { _PropertyChanged += value; }
+                remove { _PropertyChanged -= value; }
+            }
+
+            protected void OnPropertyChanged([CallerMemberName]string prop = "")
+            {
+                _PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+}
