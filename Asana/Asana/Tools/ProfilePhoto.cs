0d88dd50a8cd9c6eb97ad5b960343a6e1bc87656
Nubar Khalidova <nkh.nubarkhalid@gmail.com>
2018-08-14 03:37:05 +0300

    SignUp view

---
+++ Asana/Asana/Tools/ProfilePhoto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asana.Tools
+{
+   public class ProfilePhoto
+    {
+        public static string LoadImage()
+        {
+            OpenFileDialog img = new OpenFileDialog();
+            img.Filter = "Image jpeg(*.jpg)|*.jpg|Image png(*.png)|*.png";
+            img.DefaultExt = ".jpeg";
+           var result = img.ShowDialog();
+
+           
+            // Process open file dialog box results 
+            if (result!=DialogResult.Cancel)
+            {
+                // Open document 
+
+                return img.FileName;
+
+            }
+            return String.Empty;
+        }
+    }
+}
