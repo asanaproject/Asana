6990decda1433cdb683e1d06e175d6420c7f807d
HP <HP@DESKTOP-4QSGHJN>
2018-09-06 07:29:53 +0300

    Column and Tasks in Project

---
+++ Asana/Asana/Model/CurrentProject.cs
@@ -0,0 +1,27 @@
+﻿using Asana.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Model
+{
+    public class CurrentProject
+    {
+        private  CurrentProject() { }
+        private static CurrentProject instance;
+        public static CurrentProject Instance
+        {
+            get
+            {
+                if (instance==null)
+                {
+                    instance = new CurrentProject();
+                }
+                return Instance;
+            }
+        }
+        public Project Project { get; set; }
+    }
+}
---
+++ Asana/Asana/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Asana.Model;
+using Asana.Objects;
+
+namespace Asana.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        System.Threading.Tasks.Task Add(Task task);
+    }
+}
