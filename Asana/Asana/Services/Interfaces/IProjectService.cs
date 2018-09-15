72c1e9100a6396827c3d3ad429999c5fe9ded204
Tural Muradov <Tural_M@hotmail.com>
2018-09-15 16:05:40 +0400

    Add project view

---
+++ Asana/Asana/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Services.Interfaces
+{
+     public interface IProjectService
+    {
+       Task Add(Objects.Project project);
+    }
+}
